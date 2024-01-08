0e78823c8df6dad5d80163f518e0997317eb1230
Vladislav <VladislavKharitonovIT@gmail.com>
2024-01-08 18:50:56 +0300

    Добавить кнопку

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ root.resizable(width=False, height=False)
 linkLabel = Label(root, text='Вставте ссылку')
 linkLabel.place(x=10, y=10)
 
+beforeLink = ttk.Entry(root, width=40, font='Arial 13')
+beforeLink.place(x=10, y=30)
+
+btn = ttk.Button(root, text='Сократить')
+btn.place(relx=0.5, y=80, anchor=CENTER)
+
 
 
 root.mainloop()
